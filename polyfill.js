4e3f65086f8d4bd4cc43977a84f7e97f9b886449
Kamal Kokne <kamalkokne4@gmail.com>
2018-01-29 16:02:35 +0530

    Updated Polyfill.js
    Added bind and map polyfills.

--- polyfill.js
+++ polyfill.js
@@ -26,4 +26,47 @@
             return result;
         }
     }
+
+    if (typeof Function.prototype.bind !== 'function') {
+        Function.prototype.bind = function(that) {
+            if (typeof this !== 'function') {
+                throw new TypeError('Call bind method for Function type only');
+            }
+
+            var self = this;
+
+            var args = Array.prototype.slice.call(arguments, 1);
+            return function() {
+                self.apply(that, args);
+            }
+        }
+    }
+
+    if (typeof Array.prototype.map !== 'function') {
+        Array.prototype.map = function(callback) {
+            if (!Array.isArray(this)) {
+                return 'Type Error - Call map method for Array type';
+            }
+            if (typeof callback !== 'function') {
+                return 'Type Error - Pass function as argument in map method';
+            }
+            var that = this,
+                Obj = Object(that),
+                loop = Obj.length >>> 0,
+                i = 0, result = new Array(loop), T;
+            if (arguments.length > 1) {
+                T = arguments[1];
+            }
+            while(i < loop) {
+                var instance, instanceMap;
+                if (i in Obj) {
+                    instance = Obj[i];
+                    instanceMap = callback.call(T, instance, i, Obj);
+                    result[i] = instanceMap;
+                }
+                i++;
+            }
+            result result;
+        }
+    }
 }(window, document));
